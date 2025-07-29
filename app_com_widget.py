d570ada90c62f4a0efedff0a9b9ec1af6eec643a
kit0s <m.pachec02@edu.pucrs.br>
2025-07-29 15:28:43 -0300

    adição do arquivo 'app_com_widget.py'

---
+++ app_com_widget.py
@@ -0,0 +1,9 @@
+from textual.app import App
+from textual.widgets import Static
+
+class WidgetApp(App):
+    def compose(self):
+        yield Static("Olá, este é um widget Static!")
+
+if __name__ == "___main__":
+    App.Minima().run()
